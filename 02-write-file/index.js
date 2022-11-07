9f70200121d4e0ca86e2c95ccc2cbb66ef114be5
Mikhail Stankevich <mikhailstn1@gmail.com>
2022-11-07 11:51:51 +0300

    add solution

---
+++ 02-write-file/index.js
@@ -0,0 +1,16 @@
+const fs = require("fs");
+const path = require("path");
+const { stdout, stdin } = process;
+const output = fs.createWriteStream(path.join(__dirname, "text.txt"));
+
+stdout.write("Enter the text here\n");
+
+stdin.on("data", (data) => {
+  if (data.toString().trim("exit") == "exit") {
+    process.exit();
+  }
+  output.write(data);
+});
+
+process.on("exit", () => stdout.write("Good bye!"));
+process.on("SIGINT", () => process.exit());
